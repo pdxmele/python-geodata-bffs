6f2022d3d3d64ffdcb9a5a9a204d6ea9cd641555
pdxmele <saxbarm@gmail.com>
2014-03-16 22:57:54 -0700

    MOAR stuff

---
+++ fiona_proj.py
@@ -0,0 +1,41 @@
+import fiona
+from fiona.crs import from_epsg
+from pyproj import Proj, transform
+
+with fiona.open('ne_110m_admin_0_countries.shp', 'r') as inp:
+    
+    output_schema = inp.schema.copy()
+    p_in = Proj(inp.crs)
+
+    with fiona.collection(
+            "output_project/output_project.shp", "w",
+            crs=from_epsg(2163), 
+            driver="ESRI Shapefile", 
+            schema=output_schema
+            ) as out:
+
+        p_out = Proj(out.crs)
+        
+        for f in inp:
+            if f["properties"]["sovereignt"] != "Antarctica":
+            #if f["properties"]["sovereignt"] == "United States of America":
+                try:
+                    if f['geometry']['type'] == "Polygon":
+                        new_coords = []
+                        for ring in f['geometry']['coordinates']:
+                            x2, y2 = transform(p_in, p_out, *zip(*ring))
+                            new_coords.append(zip(x2, y2))
+                        f['geometry']['coordinates'] = new_coords
+                    elif f['geometry']['type'] == "MultiPolygon":
+                        new_coords = []
+                        inner_coords = []
+                        for polygon in f['geometry']['coordinates']:
+                            count = 0
+                            for ring in polygon:
+                                x2, y2 = transform(p_in, p_out, *zip(*ring))
+                                inner_coords.append(zip(x2, y2))
+                        new_coords.append(inner_coords)
+                        f['geometry']['coordinates'] = new_coords
+                    out.write(f)
+                except Exception, e:
+                    print "Error transforming feature " + f['id']
--- fionaexample1.py
+++ fionaexample1.py
@@ -1,5 +1,5 @@
 import fiona
-with fiona.open('ne_110m_admin_0_countries.shp', 'r') as inp:
+with fiona.open('ne_110m_admin_0_countries/ne_110m_admin_0_countries.shp', 'r') as inp:
     output_schema = inp.schema.copy()
     with fiona.collection(
             "output.geojson", "w",
@@ -8,6 +8,24 @@ with fiona.open('ne_110m_admin_0_countries.shp', 'r') as inp:
             schema=output_schema
             ) as out:
         for f in inp:
-            print f["properties"]
             if f["properties"]["sovereignt"] != "Antarctica":
                 out.write(f)
+
+
+"""
+import fiona
+with fiona.open('ne_110m_admin_0_countries.shp', 'r', \
+    encoding='Windows-1252') as inp:
+    output_schema = inp.schema.copy()
+    with fiona.collection(
+            "output.geojson", "w",
+            crs=inp.crs, 
+            driver="GeoJSON", 
+            schema=output_schema
+            ) as out:
+        for f in inp:
+            if f["properties"]["sovereignt"] != "Antarctica":
+                out.write(f)
+
+
+"""
