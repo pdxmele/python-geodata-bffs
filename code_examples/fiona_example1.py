3a48cd1bca0c929bdca426dd3e6059cfa7f8315a
Sean Gillies <sean@mapbox.com>
2014-04-01 22:06:57 -0600

    Improve Fiona examples.
    They are all excellent and don't need huge improvement. But I
    did the following:
    
    - Changed collection() to open().
    - Removed 'r' mode args since this is the default.
    - Switched to single quotes.
    - Avoid unnecessary schema copying (saves a line of code!)
    - Removed duplicate code in the (awesome!) pyproj example.
    
    I ran the modified examples to confirm they work. The stereographic
    example is really good.

--- code_examples/fiona_example1.py
+++ code_examples/fiona_example1.py
@@ -1,14 +1,13 @@
 import fiona
 
 
-with fiona.open('ne_110m_admin_0_countries.shp', 'r') as inp:
-    output_schema = inp.schema.copy()
-    with fiona.collection(
-            "output.geojson", "w",
-            crs=inp.crs, 
-            driver="GeoJSON", 
-            schema=output_schema
-            ) as out:
-        for f in inp:
-            if f["properties"]["sovereignt"] != "Antarctica":
-                out.write(f)
+with fiona.open('ne_110m_admin_0_countries.shp') as src:
+    with fiona.open(
+            'output.geojson', 'w',
+            crs=src.crs,
+            driver='GeoJSON',
+            schema=src.schema
+            ) as dst:
+        for f in src:
+            if f['properties']['sovereignt'] != 'Antarctica':
+                dst.write(f)
--- code_examples/fiona_example2.py
+++ code_examples/fiona_example2.py
@@ -5,23 +5,22 @@ import os
 ROOT_DIR = 'moves_export'
 EXAMPLE_FILE = 'moves_export/storylines_20131201_to_20131207.gpx'
 
-with fiona.open(EXAMPLE_FILE, 'r', layer='tracks') as example:
+with fiona.open(EXAMPLE_FILE, layer='tracks') as example:
     in_schema = example.schema
     in_crs = example.crs
 
-with fiona.collection(
-        "tracks_output.geojson", "w",
+with fiona.open(
+        'tracks_output.geojson', 'w',
         crs=in_crs,
-        driver="GeoJSON",
-        schema=in_schema.copy()
+        driver='GeoJSON',
+        schema=in_schema
         ) as out:
     for root, dirs, files in os.walk(ROOT_DIR):
         for file in files:
-            file_str = ROOT_DIR + "/" + file
-            if file_str[-4:len(file_str)] == ".gpx":
+            file_str = ROOT_DIR + '/' + file
+            if file_str[-4:len(file_str)] == '.gpx':
                 with fiona.open(
-                        file_str, 
-                        'r', 
+                        file_str,
                         layer='tracks'
                         ) as inp:
                     for feature in inp:
